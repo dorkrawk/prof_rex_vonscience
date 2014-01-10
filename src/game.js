b41397c9b21bdae3fcfeb1b8b2bc1999356fd982
Dave <dave.schwantes@gmail.com>
2014-01-09 21:48:57 -0800

    Initial commit

---
+++ src/game.js
@@ -0,0 +1,24 @@
+Game = {
+  map_grid: {
+    width: 32,
+    height: 18,
+    tile: {
+      width: 32,
+      height: 32
+    }
+  },
+
+  width: function() {
+    return this.map_grid.width * this.map_grid.tile.width;
+  },
+  height: function() {
+    return this.map_grid.height * this.map_grid.tile.height;
+  },
+
+  start: function() {
+    Crafty.init(Game.width(), Game.height());
+    Crafty.background('rgb(249, 223, 125)');
+
+    Crafty.scene('Loading');
+  }
+}
